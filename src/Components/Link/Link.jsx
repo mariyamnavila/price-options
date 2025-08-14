9eda900d73460c83aab4ec119d24dd08860f6348
mariyamnavila <bibimariyamnavila@gmail.com>
2025-08-14 20:21:45 +0600

    added navbar button toggle and animation

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -1,6 +1,6 @@
 const Link = ({route}) => {
     return (
-        <li className="mr-10" key={route.id}><a href={route.path}>{route.name}</a></li>
+        <li className="mr-10 hover:bg-slate-800 px-2 rounded-lg" key={route.id}><a href={route.path}>{route.name}</a></li>
     );
 };
 
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Link from "../Link/Link";
 import { RiMenu2Line } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false)
 
     const routes = [
         { id: 1, path: "/", name: "Home" },
@@ -14,11 +18,17 @@ const NavBar = () => {
 
 
     return (
-        <nav>
-            <RiMenu2Line className="text-2xl md:hidden"/>
-            <ul className="md:flex ">
+        <nav className="text-white">
+            <div className="md:hidden" onClick={() => setOpen(!open)}>
                 {
-                    routes.map(route => <Link route={route}></Link> )
+                    open === true
+                        ? <AiOutlineClose className="text-2xl " />
+                        : <RiMenu2Line className="text-2xl " />
+                }
+            </div>
+            <ul className={`md:flex duration-1000 absolute md:static ${open?'top-16':'-top-60'} bg-black px-5 py-3 ml-2 rounded-xl `}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
             </ul>
         </nav>
